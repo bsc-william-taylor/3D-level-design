af0519cd4267c378815972e90e50b3ab1fc2e88a
William Taylor <wi11berto@yahoo.co.uk>
2017-01-11 00:26:21 +0000

    adding working project

---
+++ Scholar/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject Spell;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !Spell.activeSelf)
+        {
+            var system = Spell.GetComponent<ParticleSystem>();           
+            system.Clear();
+            system.Play();
+
+            Spell.SetActive(true);
+
+            StartCoroutine(Wait(2, () =>
+            {
+                Spell.SetActive(false);
+            }));
+        }
+    }
+
+    static IEnumerator Wait(float seconds, Action callback)
+    {
+        yield return new WaitForSeconds(seconds);
+        callback();
+    }
+}
