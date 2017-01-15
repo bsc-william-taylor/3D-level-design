27d16bca0481667bfb7f057c5d2d9474d5ff429c
William Taylor <wi11berto@yahoo.co.uk>
2017-01-15 17:28:29 +0000

    adding menu scene

---
+++ Scholar/Assets/Scripts/UiEvents.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UiEvents : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+}
