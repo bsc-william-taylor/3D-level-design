6466042c65550b4cd650016e3161de1935b42008
william-taylor <wi11berto@yahoo.co.uk>
2017-03-05 15:05:31 +0000

    updating background music

--- Scholar/Assets/Scripts/NextStage.cs
+++ Scholar/Assets/Scripts/NextStage.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class NextStage : MonoBehaviour
 {
     public StateController StageController;
+    public AudioClip BattleMusic;
+
+    private bool changedMusic = false;
 
     void OnCollisionEnter(Collision col)
-    {
+    {   
         if (col.gameObject.name == "Player")
         {
             gameObject.SetActive(false);
             StageController.NextSection();
+
+            if (name == "BattleTrigger" && !changedMusic)
+            {
+                var audioSource = GameObject.Find("BackgroundMusic").GetComponent<AudioSource>();
+                audioSource.clip = BattleMusic;
+                audioSource.Play();
+                changedMusic = true;
+            }
         }
     }
 }
