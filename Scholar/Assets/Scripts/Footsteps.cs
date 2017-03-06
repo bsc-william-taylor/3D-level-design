66f10e25d0ae4de0c7fa514a354be92be59203ac
william-taylor <wi11berto@yahoo.co.uk>
2017-03-06 15:06:52 +0000

    better footsteps

--- Scholar/Assets/Scripts/Footsteps.cs
+++ Scholar/Assets/Scripts/Footsteps.cs
@@ -13,6 +13,9 @@ public class Footsteps : MonoBehaviour
 
     void Update()
     {
+
+        source.pitch = Input.GetKey(KeyCode.LeftShift) ? 1.4f : 1.0f;
+
         if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical"))
         {
             source.Play();
